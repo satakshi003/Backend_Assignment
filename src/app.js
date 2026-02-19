7f8f6493c0556f2b9e61b4422bf816035e90b9ac
satakshi003 <soumya3955@gmail.com>
2026-02-19 21:42:08 +0530

    add cookie-parser middleware and update package dependencies

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import morgan from "morgan";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/task.routes.js";
+import cookieParser from "cookie-parser";
+
+
 
 const app = express();
 
@@ -13,7 +16,7 @@ app.use("/api/v1/tasks", taskRoutes);
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-app.use(errorHandler);
+app.use(errorHandler);app.use(cookieParser());
 
 
 export default app;
